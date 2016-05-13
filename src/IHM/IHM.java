e6e78d46623da8ad22dd31dbd699d8b0b96728e1
arcaman <christopher38c@hotmail.com>
2016-05-13 09:40:49 +0200

    Commit du point d'entrée de l'application qui se trouve dans src/IHM/IHM.java

--- src/IHM/IHM.java
+++ src/IHM/IHM.java
@@ -1,5 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package IHM;
 
-public class IHM {
+import java.awt.*;
+import javax.swing.*;
 
+/**
+ *
+ * @author givaudav
+ */
+public class IHM implements Runnable {
+
+	public void run() {
+		// Creation d'une fenetre
+		JFrame frame = new JFrame("Projet Renjou");
+
+		// Un clic sur le bouton de fermeture clos l'application
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// On fixe la taille et on demarre
+		frame.setSize(500, 200);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new IHM());
+	}
 }
