a962ff441d4798163b8daa042ba065481a026e83
Alixia <lilounixette@hotmail.fr>
2016-05-17 12:51:39 +0200

    modification point-> coordonnees

--- src/Joueur/IAFacile.java
+++ src/Joueur/IAFacile.java
@@ -5,63 +5,70 @@
  */
 package Joueur;
 
-import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Observable;
 
 import Controleur.*;
 import Enum.*;
 
-
 public class IAFacile extends IA {
-	
-	public IAFacile(TypeJoueur type, int nbPion, Moteur m){
+
+	int nbLigne = 13;
+	int nbColonne = 13;
+
+	public IAFacile(TypeJoueur type, int nbPion, Moteur m) {
 		super();
 		this.m = m;
 		this.type = type;
 		this.nbPion = nbPion;
-		
+
 	}
-	
+
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
-		Point p = jouer();
-		Point pInverse = new Point(p.y, p.x);
-		//m.operationJouer(pInverse, type);
-		
-		
-	}
-	
-	public Point jouer(){
-		
+		Coordonees p = jouer();
+		// m.operationJouer(p, type);
 
-		
+	}
+
+	public Coordonees jouer() {
+
 		return null;
 	}
-	
-	public boolean caseJouable(Point p){
-		boolean retour = (m.getRenjou().getPlateauDeJeu().getPlateau()[p.x][p.y] == TypeCase.CasePionBlanc);
-		retour = retour && (m.getRenjou().getPlateauDeJeu().getPlateau()[p.x][p.y] == TypeCase.CasePionNoir);
-		retour = retour && (m.getRenjou().getPlateauDeJeu().getPlateau()[p.x][p.y] == TypeCase.CaseInjouable);
+
+	public boolean caseJouable(Coordonees p) {
+		boolean retour = (m.getRenjou().getPlateauDeJeu().getPlateau()[p.getLigne()][p.getColonne()] == TypeCase.CasePionBlanc);
+		retour = retour && (m.getRenjou().getPlateauDeJeu().getPlateau()[p.getLigne()][p.getColonne()] == TypeCase.CasePionNoir);
+		retour = retour && (m.getRenjou().getPlateauDeJeu().getPlateau()[p.getLigne()][p.getColonne()] == TypeCase.CaseInjouable);
 		return retour;
 	}
-	
-	public void modifierHeristique(Point p){
-		
+
+	public void modifierHeristique(Coordonees p) {
+		int i = p.getLigne();
+		int j = p.getColonne();
+		if (i > 0) {
+			if(j >0){
+				tabHeuristique[i][j] ++;
+			}else{
+				
+			}
+		}else{
+			
+		}
 	}
-    
-	public void initHeuristique(){
-		
-		for(int i=0; i<13; i++){
-			for(int j=0; j<13; j++){
-				Point p = new Point(i, j);
-				if(caseJouable(p)){
-					
+
+	public void initHeuristique() {
+
+		for (int i = 0; i < nbLigne; i++) {
+			for (int j = 0; j < nbColonne; j++) {
+				Coordonees p = new Coordonees(i, j);
+				if (caseJouable(p)) {
+
 				}
 			}
 		}
-		
+
 	}
-	
+
 }
