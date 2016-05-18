f0fec6ab1fa7d194a86112a315aef66a51dfe737
arcaman <christopher38c@hotmail.com>
2016-05-18 10:32:31 +0200

    externalisation des tests moteur dans le fichier TestMoteur.java plus clean de l'IHM

--- src/IHM/IHM.java
+++ src/IHM/IHM.java
@@ -5,11 +5,12 @@
  */
 package IHM;
 
-
 import java.awt.*;
 import javax.swing.*;
 
+import Controleur.Coordonnees;
 import Controleur.Moteur;
+import Enum.TypeJoueur;
 
 /**
  *
@@ -18,34 +19,31 @@ import Controleur.Moteur;
 public class IHM implements Runnable {
 	JFrame[] frames;
 	Moteur m;
- 	
-	public void run() {		
+
+	public void run() {
 		// Creation d'une fenetre
 
 		frames = new JFrame[2];
 		frames[0] = new FenetreJeu(m);
 
-		int nbJoueurs = 2;
-		Moteur donneesJeu = new Moteur(nbJoueurs);
-		donneesJeu.afficherPlateauJeu();
 		// Un clic sur le bouton de fermeture clos l'application
 		frames[0].setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// On fixe la taille et on demarre
-		frames[0].setSize(1366,768);
+		frames[0].setSize(1366, 768);
 		frames[0].setLocationRelativeTo(null);
 		frames[0].setVisible(true);
-		
+
 		frames[1] = new FenetreMenu(m);
 
 		// Un clic sur le bouton de fermeture clos l'application
 		frames[1].setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// On fixe la taille et on demarre
 
-		frames[1].setSize(1366,768);
+		frames[1].setSize(1366, 768);
 		frames[1].setLocationRelativeTo(null);
 		frames[1].setSize(500, 200);
 		frames[1].setVisible(false);
-}
+	}
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new IHM());
