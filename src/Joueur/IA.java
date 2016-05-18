15c2a1460c6ef3ca6530f41651cef5d02b4e6658
Alex <Alex@192.168.1.71>
2016-05-18 23:34:34 +0200

    Mauvais merge, correction

--- src/Joueur/IA.java
+++ src/Joueur/IA.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import Controleur.Coordonnees;
 import Controleur.Moteur;
 import Controleur.MoteurObserveur;
+import Controleur.PlateauDeJeu;
 import Enum.TypeCase;
 import Enum.TypeCouleur;
 import Enum.TypeJoueur;
@@ -33,51 +34,53 @@ public class IA extends Joueur implements MoteurObserveur{
 		}
 	}
 	
-	public boolean caseJouable(Coordonnees p) {
-		return (m.getRenjou().getPlateauDeJeu().getPlateau()[p.getLigne()][p.getColonne()] == TypeCase.Jouable);
+
+	public boolean caseJouable(Coordonnees p, PlateauDeJeu plateau) {
+		return (plateau.getPlateau()[p.getLigne()][p.getColonne()] == TypeCase.Jouable);
+
 	}
 
-	public void modifierHeristique(Coordonnees p) {
+	public void modifierHeristique(Coordonnees p, PlateauDeJeu plateau) {
 		int i = p.getLigne();
 		int j = p.getColonne();
 		if (i > 0) {
-			if(caseJouable(new Coordonnees(i-1,j))){
+			if(caseJouable(new Coordonnees(i-1,j), plateau)){
 				tabHeuristique[i-1][j] ++;
 			}
 			if(j >0){
-				if(caseJouable(new Coordonnees(i-1,j-1))){
+				if(caseJouable(new Coordonnees(i-1,j-1), plateau)){
 					tabHeuristique[i-1][j-1] ++;
 				}
 			}
 			if(j < nbColonne){
-				if(caseJouable(new Coordonnees(i-1,j+1))){
+				if(caseJouable(new Coordonnees(i-1,j+1), plateau)){
 					tabHeuristique[i-1][j+1] ++;
 				}
 			}
 		}
 		if(i < nbLigne){
-			if(caseJouable(new Coordonnees(i+1,j))){
+			if(caseJouable(new Coordonnees(i+1,j), plateau)){
 				tabHeuristique[i+1][j] ++;
 			}
 			if(j >0){
-				if(caseJouable(new Coordonnees(i+1,j-1))){
+				if(caseJouable(new Coordonnees(i+1,j-1), plateau)){
 					tabHeuristique[i+1][j-1] ++;
 				}
 			}
 			if(j < nbColonne){
-				if(caseJouable(new Coordonnees(i+1,j+1))){
+				if(caseJouable(new Coordonnees(i+1,j+1), plateau)){
 					tabHeuristique[i+1][j+1] ++;
 				}
 			}
 		}
 		
 		if(j >0){
-			if(caseJouable(new Coordonnees(i,j-1))){
+			if(caseJouable(new Coordonnees(i,j-1), plateau)){
 				tabHeuristique[i][j-1] ++;
 			}
 		}
 		if(j < nbColonne){
-			if(caseJouable(new Coordonnees(i,j+1))){
+			if(caseJouable(new Coordonnees(i,j+1), plateau)){
 				tabHeuristique[i][j+1] ++;
 			}
 		}
@@ -92,14 +95,14 @@ public class IA extends Joueur implements MoteurObserveur{
 		}
 	}
 	
-	public void initHeuristique() {
+	public void initHeuristique(PlateauDeJeu plateau) {
 
 		for (int i = 0; i < nbLigne; i++) {
 			for (int j = 0; j < nbColonne; j++) {
 				Coordonnees p = new Coordonnees(i, j);
-				if (caseJouable(p)) {
+				if (caseJouable(p, plateau)) {
 					m.printTrace(9,"appel a modifier Heuristique");
-					modifierHeristique(p);
+					modifierHeristique(p, plateau);
 				}
 			}
 		}
