37c598d531e2dd548ed2a2f71ccbfa3c3a49f2de
Alixia <lilounixette@hotmail.fr>
2016-05-18 17:34:53 +0200

    Test IAFacile (modif des dossiers de tests sur fichier)

--- src/Tests/TestIAFacile.java
+++ src/Tests/TestIAFacile.java
@@ -19,26 +19,55 @@ public class TestIAFacile {
 
 	private static String chemin = "TestsPlateauDeJeu/TestsIA/";
 	
+	
+	Moteur m;
+	int nbJoueur = 2;
+	IAFacile iaFacile;
+	
 	@Before
 	public void init() {
+		
+		m = new Moteur(nbJoueur);
+		iaFacile = new IAFacile(m,TypeJoueur.IAFacile, 60, TypeCouleur.Noir);
+		
 	}
 	
 	
 	@Test
-	public void test() {
-		
-		Moteur m;
-		int nbJoueur = 2;
-		IAFacile iaFacile;
+	public void testNoirHorizonNaif() {
 
-		m = new Moteur(nbJoueur);
-		iaFacile = new IAFacile(m,TypeJoueur.IAFacile, 60, TypeCouleur.Noir);
-		
 		PlateauDeJeu plateau= iaFacile.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirHorizonNaif");
-		System.out.println(plateau);
 		Coordonnees c = iaFacile.jouer(plateau);
-		System.out.println(c);
 		assertTrue(((c.getLigne()==7)&&(c.getColonne()==4))||((c.getLigne()==7)&&(c.getColonne()==9)));
+		
+	}
+	
+	@Test
+	public void testNoirVertNaif() {
+
+		PlateauDeJeu plateau= iaFacile.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirVertNaif");
+		Coordonnees c = iaFacile.jouer(plateau);
+		assertTrue(((c.getLigne()==4)&&(c.getColonne()==5))||((c.getLigne()==9)&&(c.getColonne()==5)));
+		
+	}
+	
+	
+	@Test
+	public void testNoirDiagoBasDroiteNaif() {
+
+		PlateauDeJeu plateau= iaFacile.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirDiagoBasDroiteNaif");
+		Coordonnees c = iaFacile.jouer(plateau);
+		assertTrue(((c.getLigne()==3)&&(c.getColonne()==2))||((c.getLigne()==8)&&(c.getColonne()==7)));
+		
+	}
+	
+	
+	@Test
+	public void testNoirDiagoBasGaucheNaif() {
+
+		PlateauDeJeu plateau= iaFacile.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirDiagoBasGaucheNaif");
+		Coordonnees c = iaFacile.jouer(plateau);
+		assertTrue(((c.getLigne()==8)&&(c.getColonne()==6))||((c.getLigne()==13)&&(c.getColonne()==1)));
 		
 	}
 
