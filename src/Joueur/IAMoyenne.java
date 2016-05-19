24093576960958902f34df93606f004d03b53bce
Alex <Alex@192.168.1.71>
2016-05-19 18:19:33 +0200

    Structure MiniMax + Heuristique sur les cases Jouables

--- src/Joueur/IAMoyenne.java
+++ src/Joueur/IAMoyenne.java
@@ -5,83 +5,261 @@
  */
 package Joueur;
 
-import Controleur.Coordonnees;
-import Controleur.Moteur;
-import Enum.TypeCouleur;
-import Enum.TypeJoueur;
+import java.util.ArrayList;
+
+import Controleur.*;
+import Enum.*;
+
 
 public class IAMoyenne extends IA {
     
 	public IAMoyenne(Moteur moteur, TypeJoueur type, int nbPion, TypeCouleur couleurJoueur){
 		super(moteur, type,nbPion,couleurJoueur);
-
-		//this.afficherTabHeuristique();
-		ameliorerTabHeuristique();
 	}
 	
 	
-	
-	
-	public void ameliorerTabHeuristique() {
-
-		for (int i = 0; i < nbLigne; i++) {
-			for (int j = 0; j < nbColonne; j++) {
-				Coordonnees p = new Coordonnees(i, j);
-				if (caseJouable(p)) {
-					tabHeuristique[i][j]--;
-				}
-			}
-		}
-
+	public TypeCase getTypeCase(TypeCouleur tc){
+		if(tc == TypeCouleur.Blanc)
+			return TypeCase.PionBlanc;
+		
+		return TypeCase.PionNoir;
 	}
 	
-	public boolean caseJouable(Coordonnees p) {
-		int i = p.getLigne();
-		int j = p.getColonne();
-		if (i > 0) {
-			if(tabHeuristique[i-1][j] > 0){
-				return true;
-			}
-			if(j > 0){
-				if(tabHeuristique[i-1][j-1] > 0){
-					 return true;
-				}
-			}
-			if(j < nbColonne){
-				if(tabHeuristique[i-1][j+1] > 0){
-					return true;
-				}
-			}
-		}
-		if(i < nbLigne){
-			if(tabHeuristique[i+1][j] > 0){
-				return true;
-			}
-			if(j >0){
-				if(tabHeuristique[i+1][j-1] > 0){
-					return true;
-				}
-			}
-			if(j < nbColonne){
-				if(tabHeuristique[i+1][j+1] > 0){
-					return true;
+	public TypeCase getAutreTypeCase(TypeCouleur tc){
+		if(tc == TypeCouleur.Blanc)
+			return TypeCase.PionNoir;
+		
+		return TypeCase.PionBlanc;
+	}
+	
+	public TypeCase autreTypeCase(TypeCase tc){
+		if(tc == TypeCase.PionBlanc)
+			return TypeCase.PionNoir;
+		
+		return TypeCase.PionBlanc;
+	}	
+	
+
+	public void Jouer(){
+		m.printTrace(8, "Dans Jouer IAMoyenne");
+		
+		PlateauDeJeu pdj =  m.getRenjou().getPlateauDeJeu().clone();
+		
+		m.printTrace(19, afficherTabHeuristique());
+		initHeuristique(pdj);
+
+		int profondeur = 3;
+		int valeur = -100000;
+		TypeCase tc =  this.getTypeCase(this.couleur);
+		ArrayList<Coordonnees> Coups = new ArrayList<Coordonnees>();
+		// parcours des coups jouable
+
+		for(int i=0;i<nbLigne;i++){
+			for(int j=0;j<nbColonne;j++){
+				Coordonnees c = new Coordonnees(i,j);
+				//Heuristique, on ne cherche pas a jouer a plus de 2 cases d'un pion existant
+				if(EstJouable(pdj, c)){
+					pdj.ajouter(c,tc);
+					int valeurtemp = EvaluerCoupAdversaire(pdj,profondeur -1, this.autreTypeCase(tc));
+					if(valeurtemp == valeur){
+						Coups.add(c);
+					}
+					else if(valeurtemp > valeur){
+						Coups.clear();
+						Coups.add(c);
+						valeur = valeurtemp;
+					}
+					
 				}
 			}
 		}
 		
-		if(j >0){
-			if(tabHeuristique[i][j-1] > 0){
-				 return true;
+		
+	}
+	
+	private int Evaluation(PlateauDeJeu pdj) {
+		
+		if(this.couleur == TypeCouleur.Blanc)
+			return EvaluationSansTabous(pdj);
+		
+		return EvaluationAvecTabous(pdj);
+	}
+
+	public int EvaluationSansTabous(PlateauDeJeu pdj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int EvaluationAvecTabous(PlateauDeJeu pdj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public int EvaluerCoupAdversaire(PlateauDeJeu pdj, int profondeur, TypeCase tc) {
+		if(profondeur == 0 || PartieFinie())
+			return Evaluation(pdj);
+		
+		int valeur = 10000;
+		for(int i=0;i<nbLigne;i++){
+			for(int j=0;j<nbColonne;j++){
+				Coordonnees c = new Coordonnees(i,j);
+				//Heuristique, on ne cherche pas a jouer a plus de 2 cases d'un pion existant
+				if(EstJouable(pdj, c)){
+					pdj.ajouter(c,tc);
+					valeur =  Math.min(valeur, EvaluerCoupIA(pdj,profondeur -1, this.autreTypeCase(tc)));
+				}
 			}
 		}
-		if(j < nbColonne){
-			if(tabHeuristique[i][j+1] > 0){
-				 return true;
+		return valeur;
+	}
+
+	public int EvaluerCoupIA(PlateauDeJeu pdj, int profondeur, TypeCase tc) {
+		if(profondeur == 0 || PartieFinie())
+			return Evaluation(pdj);
+			
+		int valeur = -10000;
+		for(int i=0;i<nbLigne;i++){
+			for(int j=0;j<nbColonne;j++){
+				Coordonnees c = new Coordonnees(i,j);
+				//Heuristique, on ne cherche pas a jouer a plus de 2 cases d'un pion existant
+				if(EstJouable(pdj, c)){
+					pdj.ajouter(c,tc);
+					valeur =  Math.max(valeur, EvaluerCoupAdversaire(pdj,profondeur -1, this.autreTypeCase(tc)));
+				}
 			}
+		}
+		return valeur;
+	}
+	
+	private boolean PartieFinie() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
+	public boolean EstJouable(PlateauDeJeu pdj, Coordonnees c) {
+		if(EstBlancOuNoir(pdj,c))
+			return true;
+		
+		return this.PionAlentourRecursif(pdj, c, 1);
+	}
+
+	public boolean PionAlentour(PlateauDeJeu pdj, Coordonnees p, int profondeur){
+		m.printTrace(66, "Dans pionAlentour");
+		if(EstBlancOuNoir(pdj, p)){
+			m.printTrace(66, "true (pionAlentour => EstBlancOuNoir)");
+			return true;
+		}else if(PionAlentourRecursif(pdj, p, profondeur -1)){
+			m.printTrace(66, "true (pionAlentour => caseJouable)");
+			return true;
 		}
 		
+		m.printTrace(66, "false");
 		return false;
 	}
 	
+	public boolean PionAlentourRecursif(PlateauDeJeu pdj, Coordonnees p, int profondeur) {
+		m.printTrace(66, "Dans caseJouable");	
+		
+		if (profondeur < 0){
+			m.printTrace(66, "profondeur < 0");
+			return false;
+		}
+		
+		int i = p.getLigne();
+		int j = p.getColonne();
+
+		if (i > 0) {
+			m.printTrace(66, "i > 0");
+			if(PionAlentour(pdj, new Coordonnees(i-1,j), profondeur)){
+				return true;
+			}
+			
+			if(j > 0){
+				m.printTrace(66, "j > 0");
+				if(PionAlentour(pdj, new Coordonnees(i-1,j-1), profondeur)){
+					 return true;	
+				}
+			}
+			
+			if(j < nbColonne -1){
+				m.printTrace(66, "j < nbColonne -1");
+				if(PionAlentour(pdj, new Coordonnees(i-1,j+1), profondeur)){
+					return true;
+				}
+			}
+			
+		}
+		
+		if(i < nbLigne -1){
+			m.printTrace(66, "i < nbLigne -1");
+			if(PionAlentour(pdj, new Coordonnees(i+1,j), profondeur)){
+				return true;
+			}
+			
+			if(j >0){
+				m.printTrace(66, "j > 0");
+				if(PionAlentour(pdj, new Coordonnees(i+1,j-1), profondeur)){
+					return true;
+				}
+			}
+				
+			if(j < nbColonne -1){
+				m.printTrace(66, "j < nbColonne -1");
+				if(PionAlentour(pdj, new Coordonnees(i+1,j+1), profondeur)){
+					return true;
+				}
+			}
+				
+			
+		}
+		
+		if(j >0){
+			m.printTrace(66, "j > 0");
+			if(PionAlentour(pdj, new Coordonnees(i,j-1), profondeur)){
+				 return true;
+			}
+		}
+			
+		
+		if(j < nbColonne -1){
+			m.printTrace(66, "nbColonne -1");
+			if(PionAlentour(pdj, new Coordonnees(i,j+1), profondeur)){
+				 return true;
+			}
+		}
+			
+		
+		m.printTrace(66, "false (caseJouable)");
+		return false;
+	}
+
+	public boolean EstBlancOuNoir(PlateauDeJeu pdj, Coordonnees c){
+		m.printTrace(66, "Dans EstBlancOuNoir");
+		return pdj.getTypeCaseTableau(c) == TypeCase.PionBlanc || pdj.getTypeCaseTableau(c) == TypeCase.PionNoir; 
+	}
+
+	
+
+
+	
+	@Override
+	public void actualiser() {
+		m.printTrace(3, "IAMoyenne notifiee");
+		
+		if(!AmonTour()){
+			m.printTrace(9, "Ce n'est pas a mon tour de jouer");
+			return;
+		}
+		
+		Jouer();
+	}
+	
+	public boolean AmonTour(){
+		return m.getRenjou().getJoueurs()[m.getRenjou().getJoueurCourant()] == this;
+	}
+
 	
 }
--- src/Tests/TestIAMoyenne.java
+++ src/Tests/TestIAMoyenne.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Controleur.Coordonnees;
 import Controleur.Moteur;
+import Controleur.PlateauDeJeu;
+import Enum.TypeCase;
 import Enum.TypeCouleur;
 import Enum.TypeJoueur;
 import Joueur.IA;
@@ -18,14 +21,38 @@ public class TestIAMoyenne {
 	
 	@Test
 	public void test() {
-		//Moteur m = new Moteur(TypeJoueur.Humain, TypeJoueur.Humain);
-		//IA ia1 = new IA(m, TypeJoueur.IAMoyenne, 60, TypeCouleur.Blanc);
-		//IA ia2 = new IA(m, TypeJoueur.IAMoyenne, 60, TypeCouleur.Blanc);
-		
-		//ia1.afficherTabHeuristique();
+		Moteur m = new Moteur(TypeJoueur.IAMoyenne, TypeJoueur.Humain);
+		PlateauDeJeu pdj = m.getRenjou().getPlateauDeJeu();
+		IAMoyenne ia = (IAMoyenne) m.getRenjou().getJoueurs()[0];
+		m.setNiveauTrace(10);
 
-		//ia.afficherTabHeuristique();
-		assertTrue(true);
+		Coordonnees centre = new Coordonnees(5,5);
+		pdj.ajouter(centre, TypeCase.PionNoir);
+		
+		for(int i=centre.getLigne() -2;i<centre.getLigne()+2;i++){
+			for(int j=centre.getColonne() -2;j<centre.getColonne() +2;j++){
+				assertTrue(ia.EstJouable(pdj, new Coordonnees(i,j)));
+			}
+		}
+		
+		
+		for(int i=0;i<pdj.getLignes();i++){
+			for(int j=0;j<pdj.getColonnes();j++){
+				if(i == centre.getLigne() && j == centre.getColonne()){
+					// spec a expliciter
+					//assertFalse(ia.caseJouable(pdj, new Coordonnees(i,j), 1));
+				}
+				else if((i <= centre.getLigne() +2 && i >= centre.getLigne() -2) && (j <= centre.getColonne() +2 && j >= centre.getColonne() -2)){
+					assertTrue(ia.EstJouable(pdj, new Coordonnees(i,j)));
+				}
+				else{
+					assertFalse(ia.EstJouable(pdj, new Coordonnees(i,j)));
+				}
+				
+			}
+		}
+
+
 	}
 
 }
