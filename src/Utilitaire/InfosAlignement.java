d3bf570995ad074bf1276dce180e879d63174e9b
Alex <Alex@192.168.1.71>
2016-05-23 12:34:47 +0200

    Ajout InfosAlignement + tests (non fonctionnel)

---
+++ src/Utilitaire/InfosAlignement.java
@@ -0,0 +1,122 @@
+package Utilitaire;
+
+import Controleur.*;
+import Enum.*;
+
+public class InfosAlignement {
+
+	int nbNoir;
+	int nbBlanc;
+	boolean noirLibre;
+	boolean blancLibre;
+	boolean caseVideNoir;
+	boolean caseVideBlanc;
+	private int ligne;
+	private int colonne;
+	private TypeDirection direction;
+	
+	int nbLignes;
+	int nbColonnes;
+
+	public InfosAlignement(PlateauDeJeu pdj, Coordonnees c, TypeDirection td) {
+		nbNoir = 0;
+		nbBlanc = 0;
+		caseVideNoir = false;
+		caseVideBlanc = false;
+		noirLibre = true;
+		blancLibre = true;
+		ligne = c.getLigne();
+		colonne = c.getColonne();
+		direction = td;
+		nbLignes = 15;
+		nbColonnes = 15;
+		
+		
+		boolean noirFini = false;
+		boolean blancFini = false;
+		boolean bordure = !incremente();
+			
+		Log.print(795, "0 - "+ (!bordure || (noirFini && blancFini)) +" - bordure= " + bordure + " noirFini= " + noirFini + " blancFini= "+ blancFini + " ligne= " +ligne+ " colonne= " + colonne);
+		
+		while(!bordure && (!noirFini || !blancFini)){
+			Log.print(795, "1 - "+ (!bordure || (noirFini && blancFini)) +" - bordure= " + bordure + " noirFini= " + noirFini + " blancFini= "+ blancFini + " ligne= " +ligne+ " colonne= " + colonne);
+
+			TypeCase tc = pdj.getTypeCaseTableau(new Coordonnees(ligne,colonne));
+			if(tc == TypeCase.PionBlanc){
+				noirFini = true;
+				noirLibre = false;
+				nbBlanc++;
+			}else if(tc == TypeCase.PionNoir){
+				blancFini = true;
+				blancLibre = false;
+				nbNoir++;
+			}else if(tc == TypeCase.Jouable){
+				
+				if(!caseVideNoir){
+					caseVideNoir = true;
+				}else{
+					noirFini = true;
+				}
+				
+				if(!caseVideBlanc){
+					caseVideBlanc = true;
+				}else{
+					blancFini = true;
+				}
+				
+				
+			}
+			Log.print(795, "2 - "+ (bordure || (noirFini && blancFini)) +" - bordure= " + bordure + " noirFini= " + noirFini + " blancFini= "+ blancFini + " ligne= " +ligne+ " colonne= " + colonne);
+			bordure = incremente();
+			Log.print(795, "3 - "+ (bordure || (noirFini && blancFini)) +" - bordure= " + bordure + " noirFini= " + noirFini + " blancFini= "+ blancFini + " ligne= " +ligne+ " colonne= " + colonne);
+
+		}
+		
+		if(bordure){
+			noirLibre = false;
+			blancLibre = false;
+		}
+		
+		
+		
+	}
+
+	private boolean incremente(){
+		switch(direction){
+		case Gauche:
+			colonne--;
+			return colonne >= 0;
+		case Droite:
+			colonne++;
+			return colonne < nbColonnes;
+		case Haut:
+			ligne--;
+			return ligne >= 0;
+		case Bas:
+			ligne++;
+			return ligne < nbLignes;		
+		case DiagonaleHautGauche:
+			ligne--;
+			colonne--;
+			return ligne >= 0 && colonne >= 0;
+		case DiagonaleHautDroite:
+			ligne--;
+			colonne++;
+			return ligne >= 0 && ligne < nbLignes;
+		case DiagonaleBasGauche:
+			ligne++;
+			colonne--;
+			return ligne < nbLignes && colonne >= 0;
+		case DiagonaleBasDroite:
+			ligne++;
+			colonne++;
+			return ligne < nbLignes && colonne < nbColonnes;
+		default:
+			return false;
+		}
+
+	}
+
+}
+
+
