bb11446913374a2a03f5ce586cba3e0179d38f8d
Alixia <lilounixette@hotmail.fr>
2016-05-24 14:01:30 +0200

    MAJ testIAFacile

--- src/Tests/TestIAFacile.java
+++ src/Tests/TestIAFacile.java
@@ -32,42 +32,42 @@ public class TestIAFacile {
 	}
 	
 	
-	@Test
-	public void testNoirGagnantHorizonNaif() {
-
-		PlateauDeJeu plateau= iaFacileNoire.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirHorizonNaif");
-		Coordonnees c = iaFacileNoire.jouer(plateau);
-		assertTrue(((c.getLigne()==7)&&(c.getColonne()==4))||((c.getLigne()==7)&&(c.getColonne()==9)));
-		
-	}
-	
-	@Test
-	public void testNoirGagnantVertNaif() {
-
-		PlateauDeJeu plateau= iaFacileNoire.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirVertNaif");
-		Coordonnees c = iaFacileNoire.jouer(plateau);
-		assertTrue(((c.getLigne()==4)&&(c.getColonne()==5))||((c.getLigne()==9)&&(c.getColonne()==5)));
-		
-	}
-	
-	@Test
-	public void testNoirGagnantDiagoBasDroiteNaif() {
-
-		PlateauDeJeu plateau= iaFacileNoire.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirDiagoBasDroiteNaif");
-		Coordonnees c = iaFacileNoire.jouer(plateau);
-		assertTrue(((c.getLigne()==3)&&(c.getColonne()==2))||((c.getLigne()==8)&&(c.getColonne()==7)));
-		
-	}
-	
-	@Test
-	public void testNoirGagnantDiagoBasGaucheNaif() {
-
-		PlateauDeJeu plateau= iaFacileNoire.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirDiagoBasGaucheNaif");
-		Coordonnees c = iaFacileNoire.jouer(plateau);
-		assertTrue(((c.getLigne()==8)&&(c.getColonne()==6))||((c.getLigne()==13)&&(c.getColonne()==1)));
-		
-	}
-	
+//	@Test
+//	public void testNoirGagnantHorizonNaif() {
+//
+//		PlateauDeJeu plateau= iaFacileNoire.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirHorizonNaif");
+//		Coordonnees c = iaFacileNoire.jouer(plateau);
+//		assertTrue(((c.getLigne()==7)&&(c.getColonne()==4))||((c.getLigne()==7)&&(c.getColonne()==9)));
+//		
+//	}
+//	
+//	@Test
+//	public void testNoirGagnantVertNaif() {
+//
+//		PlateauDeJeu plateau= iaFacileNoire.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirVertNaif");
+//		Coordonnees c = iaFacileNoire.jouer(plateau);
+//		assertTrue(((c.getLigne()==4)&&(c.getColonne()==5))||((c.getLigne()==9)&&(c.getColonne()==5)));
+//		
+//	}
+//	
+//	@Test
+//	public void testNoirGagnantDiagoBasDroiteNaif() {
+//
+//		PlateauDeJeu plateau= iaFacileNoire.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirDiagoBasDroiteNaif");
+//		Coordonnees c = iaFacileNoire.jouer(plateau);
+//		assertTrue(((c.getLigne()==3)&&(c.getColonne()==2))||((c.getLigne()==8)&&(c.getColonne()==7)));
+//		
+//	}
+//	
+//	@Test
+//	public void testNoirGagnantDiagoBasGaucheNaif() {
+//
+//		PlateauDeJeu plateau= iaFacileNoire.getMoteur().getRenjou().getPlateauDeJeu().charger(chemin + "ProchainCoupGagnantNoirDiagoBasGaucheNaif");
+//		Coordonnees c = iaFacileNoire.jouer(plateau);
+//		assertTrue(((c.getLigne()==8)&&(c.getColonne()==6))||((c.getLigne()==13)&&(c.getColonne()==1)));
+//		
+//	}
+//	
 //	//a implementer avec fichier ou ya un point et ou il n'y en a pas
 //	@Test
 //	public void testNoirGagnantHorizon() {
